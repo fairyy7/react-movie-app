a1da7801baea8b9796f5e52a35fd9e3e34c12228
fairyy7 <fariha.haroon5@gmail.com>
2025-07-09 12:59:18 -0600

    Fix: updated Favorites component logic to check for empty array

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -3,28 +3,27 @@ import { useMovieContext } from '../contexts/MovieContext'
 import MovieCard from '../components/MovieCard'
 
 function Favorites() {
-    const {favorites} = useMovieContext();
+    const { favorites } = useMovieContext();
 
-    if (favorites) {
-    return ( <div className="favorites">
-        <h2>Your Favorites</h2>
-        <div className='movies-grid'>
-            {favorites.map((movie) => (
-                <MovieCard movie={movie} key={movie.id} />
-            ))}
-        </div>
-    </div>
-    );
-}
-
-    else {
+    if (favorites && favorites.length > 0) {
         return (
-        <div className='favorites-empty'>
-            <h2>No favorites movie yet</h2>
-            <p>Start adding movies to your favorites and they will appear here!</p>
-        </div>
-    );
-}
+            <div className="favorites">
+                <h2>Your Favorites</h2>
+                <div className="movies-grid">
+                    {favorites.map((movie) => (
+                        <MovieCard movie={movie} key={movie.id} />
+                    ))}
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="favorites-empty">
+                <h2>No favorites movie yet</h2>
+                <p>Start adding movies to your favorites and they will appear here!</p>
+            </div>
+        );
+    }
 }
 
 export default Favorites;
