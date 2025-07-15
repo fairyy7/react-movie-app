657f30f83e18f60486da0d6fabe0ef4f09530b43
fairyy7 <fariha.haroon5@gmail.com>
2025-07-15 17:46:21 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ReactJS-Movie', // 👈 This must match your repo name if hosted on GitHub Pages
+  base: '/ReactJS-Movie/', // 👈 This must match your repo name if hosted on GitHub Pages
 
 })
